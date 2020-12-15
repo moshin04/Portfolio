d1e46bf9d755faf3bfea1c96fb53f748262fa6bf
unknown <moshin.i.g@gmail.com>
2020-12-15 09:24:53 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAZpNOmXkl5JSFWdeX0Om2D4LaQyxYUoR4",
+    authDomain: "moshin-gachinmal.firebaseapp.com",
+    projectId: "moshin-gachinmal",
+    storageBucket: "moshin-gachinmal.appspot.com",
+    messagingSenderId: "28864198548",
+    appId: "1:28864198548:web:58abd1137294782b784181",
+    measurementId: "G-GQDSWQ0JLH"
+  };
